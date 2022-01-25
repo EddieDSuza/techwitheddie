0b994038d5452c799ff11df97a415504af36e2d7
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2022-01-25 19:31:16 -0300

    Update sonoff.sh

--- sonoff.sh
+++ sonoff.sh
@@ -22,25 +22,25 @@ echo "Setting Up Your Hub W/ Zigbee2MQTT Sonoff - Ver 1.0"
 echo "----------------------------------------------------------------"
 echo " "
 # System Upgrade
-echo "----------------------------------------------------------------"
-echo "Commence System Upgrade"
-echo "----------------------------------------------------------------"
+echo " "
+echo " >>>>> Commence System Upgrade <<<<<"
+echo " "
 sudo apt -y update
 sudo apt -y upgrade
-echo "----------------------------------------------------------------"
-echo "System Upgrade Completed"
-echo "----------------------------------------------------------------"
+echo " "
+echo ">>>>> System Upgrade Completed <<<<<"
 echo " "
 # MQTT setup
-echo "----------------------------------------------------------------"
-echo "Commence MQTT Setup"
-echo "----------------------------------------------------------------"
+echo " "
+echo ">>>>> Commence MQTT Setup <<<<<"
+echo " "
 sudo apt-get install mosquitto -y
 sudo apt-get install mosquitto-clients
-echo "----------------------------------------------------------------"
-echo "Create mosquitto.conf file"
-echo "----------------------------------------------------------------"
+echo " "
+echo ">>>>> Create mosquitto.conf file <<<<<"
+echo " "
 cat > /etc/mosquitto/mosquitto.conf <<EOL
+
 # Place your local configuration in /etc/mosquitto/conf.d/
 #
 # A full description of the configuration file is at
@@ -59,28 +59,26 @@ allow_anonymous false
 password_file /etc/mosquitto/pwfile
 listener 1883
 EOL
-echo "----------------------------------------------------------------"
-echo "mosquitto.conf file created"
-echo "----------------------------------------------------------------"
+echo " "
+echo ">>>>> mosquitto.conf file created <<<<<"
 echo " "
 # Z2M setup
-echo "----------------------------------------------------------------"
-echo "Commence Zigbee2MQTT Setup"
-echo "----------------------------------------------------------------"
+echo ">>>>> Commence Zigbee2MQTT Setup <<<<<"
 echo " "
-echo "Install Node.js and required dependencies"
+echo ">>>>> Install Node.js and Dependencies <<<<<"
 sudo apt-get install -y nodejs npm git make g++ gcc
 echo " "
-echo "Clone Zigbee2MQTT repository"
+echo ">>>>> Clone Zigbee2MQTT repository <<<<<"
 git clone https://github.com/Koenkk/zigbee2mqtt.git
 sudo mv zigbee2mqtt /opt/zigbee2mqtt
 echo " "
-echo "Install dependencies (as user "pi")"
+echo ">>>>> Install dependencies (as user "pi" or "hoobs" <<<<<)"
 cd /opt/zigbee2mqtt
-npm ci
+npm ci -production
 echo " "
-echo "Create Configuration.Yaml"
+echo ">>>>> Create configuration.yaml <<<<<"
 cat > /opt/zigbee2mqtt/data/configuration.yaml <<EOL
+
 # MQTT settings
 mqtt:
   # MQTT base topic for Zigbee2MQTT MQTT messages
@@ -99,9 +97,9 @@ advanced:
   network_key: GENERATE
 EOL
 echo " "
-echo "Configuration.yaml file created"
+echo ">>>>> configuration.yaml file created <<<<<"
 echo " "
-echo "Create Running as a daemon with systemctl"
+echo ">>>>> Enable daemon with systemctl <<<<<"
 cat > /etc/systemd/system/zigbee2mqtt.service <<EOL
 [Unit]
 Description=zigbee2mqtt
@@ -121,7 +119,11 @@ WantedBy=multi-user.target
 EOL
 echo " "
 echo "Zigbee2MQTT Installed"
+echo " "
 echo "Proceed to Configure MQTT Username & Password"
+echo " "
 echo "Then Proceed to Configure Zigbee2MQTT"
-echo "The End"
+echo " "
+echo ">>>>> Start Zigbee2MQTT automatically on boot <<<<<"
+echo " "
 sudo systemctl enable zigbee2mqtt.service
