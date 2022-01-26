7e11c399d72631c236887d030221fc177cffd83e
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2022-01-26 16:45:58 -0300

    Update sonoff.sh

--- sonoff.sh
+++ sonoff.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-##################################################################################################
-# zigbee2mqtt.                                                                                   #
-# Copyright (C) 2021 Tech With Eddie                                                             #
-#                                                                                                #
-# This program is free software: you can redistribute it and/or modify                           #
-# it under the terms of the GNU General Public License as published by                           #
-# the Free Software Foundation, either version 3 of the License, or                              #
-# (at your option) any later version.                                                            #
-#                                                                                                #
-# This program is distributed in the hope that it will be useful,                                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  #
-# GNU General Public License for more details.                                                   #
-#                                                                                                #
-# You should have received a copy of the GNU General Public License                              #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.                          #
-##################################################################################################
 echo " "
 echo "----------------------------------------------------------------"
 echo "Setting Up Your Hub W/ Zigbee2MQTT Sonoff - Ver 1.0"
@@ -29,45 +12,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 echo " "
 echo ">>>>> System Upgrade Completed <<<<<"
 echo " "
-# MQTT setup
-echo " "
-echo ">>>>> Commence MQTT Setup <<<<<"
-echo " "
-sudo apt-get install mosquitto -y
-sudo apt-get install mosquitto-clients
-echo " "
-echo ">>>>> Create mosquitto.conf file <<<<<"
-echo " "
-cat > /etc/mosquitto/mosquitto.conf <<EOL
-
-# Place your local configuration in /etc/mosquitto/conf.d/
-#
-# A full description of the configuration file is at
-# /usr/share/doc/mosquitto/examples/mosquitto.conf.example
-
-pid_file /var/run/mosquitto.pid
-
-persistence true
-persistence_location /var/lib/mosquitto/
-
-log_dest file /var/log/mosquitto/mosquitto.log
-
-include_dir /etc/mosquitto/conf.d
-
-allow_anonymous false
-
-password_file /etc/mosquitto/pwfile
-
-listener 1883
-EOL
-echo " "
-echo ">>>>> mosquitto.conf file created <<<<<"
-echo " "
 # Z2M setup
 echo ">>>>> Commence Zigbee2MQTT Setup <<<<<"
-echo " "
-echo ">>>>> Install Node.js and Dependencies <<<<<"
-sudo apt-get install -y nodejs npm git make g++ gcc
 echo " "
 echo ">>>>> Clone Zigbee2MQTT repository <<<<<"
 sudo git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
@@ -102,7 +48,7 @@ mqtt:
 # Serial settings
 serial:
   # Location of the adapter (see first step of this guide)
-  port: /dev/ttyAMA0
+  port: /dev/ttyUSB0
 
 frontend:
   # Optional, default 8080 or you can use your own as well.
