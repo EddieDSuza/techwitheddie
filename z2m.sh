0fe7bc338c5fa9126e466d092fcd6efe30a5666e
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2022-01-27 07:00:19 -0300

    Update and rename sonoff.sh to z2m.sh

--- z2m.sh
+++ z2m.sh
@@ -1,49 +1,9 @@
 #!/bin/bash
-echo " "
 echo "----------------------------------------------------------------"
-echo "Setting Up Your Hub W/ Zigbee2MQTT Sonoff - Ver 1.0"
+echo "Setting Up Your Hub W/ Zigbee2MQTT - Ver 1.0"
 echo "----------------------------------------------------------------"
-echo " "
-# System Upgrade
-echo " "
-echo " >>>>> Commence System Upgrade <<<<<"
-echo " "
-sudo apt-get update && sudo apt-get upgrade -y
-echo " "
-echo ">>>>> System Upgrade Completed <<<<<"
-echo " "
-# System Upgrade
-echo " "
-echo " >>>>> Commence MQTT Setup <<<<<"
-echo " "
-sudo apt-get install mosquitto -y
-sudo apt-get install mosquitto-clients
-cat > /opt/zigbee2mqtt/data/configuration.yaml <<EOL
-# Place your local configuration in /etc/mosquitto/conf.d/
-# A full description of the configuration file is at
-# /usr/share/doc/mosquitto/examples/mosquitto.conf.example
-
-pid_file /run/mosquitto/mosquitto.pid
-
-persistence true
-
-persistence_location /var/lib/mosquitto/
-
-log_dest file /var/log/mosquitto/mosquitto.log
-
-include_dir /etc/mosquitto/conf.d
-
-allow_anonymous true
-
-listener 1883
-
-EOL
-echo " "
-echo ">>>>> MQTT Installed <<<<<"
 echo " "
 # Z2M setup
-echo ">>>>> Commence Zigbee2MQTT Setup <<<<<"
-echo " "
 echo ">>>>> Clone Zigbee2MQTT repository <<<<<"
 sudo git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
 sudo chown -R pi:pi /opt/zigbee2mqtt
@@ -115,3 +75,4 @@ echo " "
 echo ">>>>> Start Zigbee2MQTT automatically on boot <<<<<"
 echo " "
 sudo systemctl enable zigbee2mqtt.service
+sudo reboot
