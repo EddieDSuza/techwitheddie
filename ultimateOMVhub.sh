f7c7d0bca60ada9d1bac9dc58f1848cee106fbbf
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2022-07-25 05:54:34 -0300

    removed portioner

--- ultimateOMVhub.sh
+++ ultimateOMVhub.sh
@@ -31,17 +31,6 @@ echo "----------------------------------------------------------------"
 echo " "
 echo " "
 echo " "
-# Portainer setup
-echo "----------------------------------------------------------------"
-echo "Commence Portainer Setup"
-echo "----------------------------------------------------------------"
-sudo docker run -d -p 9000:9000 --name=portainer --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-echo "----------------------------------------------------------------"
-echo "Portainer Interface is reachable at homebridge.local:9000"
-echo "----------------------------------------------------------------"
-echo " "
-echo " "
-echo " "
 # Watch Tower setup
 echo "----------------------------------------------------------------"
 echo "Commence Watch Tower Setup"
