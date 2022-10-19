69bdc10e2f14999b90dcbe6e343f16cfbd0d445a
Eddie dSuZa <52710883+EddieDSuza@users.noreply.github.com>
2022-10-19 17:05:17 -0300

    Update dsm7homebridgeZ2M.sh

--- dsm7homebridgeZ2M.sh
+++ dsm7homebridgeZ2M.sh
@@ -48,12 +48,6 @@ echo "----------------------------------------------------------------"
 echo " "
 echo " "
 echo " "
-# Z2M configuration.yaml 
-echo "----------------------------------------------------------------"
-echo "Transfer Zigbee2MQTT Config"
-echo "----------------------------------------------------------------"
-sudo rm -rf /volume1/docker/zigbee2mqtt/data/configuration.yaml
-wget https://raw.githubusercontent.com/EddieDSuza/techwitheddie/main/configuration.yaml -P /volume1/docker/zigbee2mqtt/data
 echo "----------------------------------------------------------------"
 echo "Transfer Zigbee2MQTT Config Complete"
 echo "----------------------------------------------------------------"
