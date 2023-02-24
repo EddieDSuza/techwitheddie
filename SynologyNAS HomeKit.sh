c7e86f945387b107896c69f6bed843e374eca2be
Eddie dSuZa <52710883+EddieDSuza@users.noreply.github.com>
2023-02-24 11:52:28 -0300

    Update SynologyNAS HomeKit.sh

--- SynologyNAS HomeKit.sh
+++ SynologyNAS HomeKit.sh
@@ -8,6 +8,17 @@ sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -
 
 sudo docker run --name="watchtower" -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
 
+# heimdall setup
+
+sudo mkdir /volume1/docker/heimdall
+id $user
+sudo docker run --name=heimdall -d --restart unless-stopped -v /volume1/docker/heimdall:/config -e PGID=1000 -e PUID=1000 -p 8201:80 -p 8200:443 linuxserver/heimdall
+
+# Scrypted setup
+
+sudo mkdir /volume1/docker/scrypted
+sudo docker run --name="scrypted" --network host -d --restart unless-stopped -v /volume1/docker/scrypted:/server/volume koush/scrypted
+
 # MQTT Setup
 
 sudo mkdir /volume1/docker/mosquitto
@@ -23,13 +34,4 @@ sudo mkdir /volume1/docker/zigbee2mqtt/data
 sudo wget https://raw.githubusercontent.com/EddieDSuza/techwitheddie/main/configuration.yaml -P /volume1/docker/zigbee2mqtt/data
 sudo docker run -itd --name="Zigbee2MQTT" --net=host -e TZ=America/Sao_Paulo --device=/dev/ttyUSB0 --restart=always -v /volume1/docker/zigbee2mqtt/data:/app/data koenkk/zigbee2mqtt:latest
 
-# Scrypted setup
 
-sudo mkdir /volume1/docker/scrypted
-sudo docker run --name="scrypted" --network host -d --restart unless-stopped -v /volume1/docker/scrypted:/server/volume koush/scrypted
-
-# heimdall setup
-
-sudo mkdir /volume1/docker/heimdall
-id $user
-sudo docker run --name=heimdall -d --restart unless-stopped -v /volume1/docker/heimdall:/config -e PGID=1000 -e PUID=1000 -p 8201:80 -p 8200:443 linuxserver/heimdall
