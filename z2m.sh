03fcfedddbcd076df5da4e116035923e90db4afc
cegareli <31626429+cegareli@users.noreply.github.com>
2023-04-08 19:35:05 -0400

    Update z2m.sh

--- z2m.sh
+++ z2m.sh
@@ -35,7 +35,7 @@ mqtt:
   # MQTT base topic for Zigbee2MQTT MQTT messages
   base_topic: zigbee2mqtt
   # MQTT server URL
-  server: 'mqtt://192.168.8.169:1883'
+  server: 'mqtt://192.168.1.38:1883'
   # MQTT server authentication, uncomment if required:
   # user: mqtt
   # password: mqtt
@@ -49,7 +49,7 @@ frontend:
   # Optional, default 8080 or you can use your own as well.
   port: 8080
   # IP address of the device running Zigbee2MQTT
-  host: 192.168.8.169
+  host: 192.168.1.38
 advanced:
   log_level: debug
   network_key: GENERATE
