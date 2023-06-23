9cf19ecd97cefb4b8fcd8b82b68a8e3e2296d4f9
Eddie dSuZa <52710883+EddieDSuza@users.noreply.github.com>
2023-06-23 09:30:13 -0300

    Create raspbee2.sh

---
+++ raspbee2.sh
@@ -0,0 +1,16 @@
+# Edit Boot Config
+sudo nano /boot/config.txt
+
+# Uncomment some or all of these to enable the optional hardware interfaces
+dtparam=i2c_arm=on
+dtparam=i2s=on
+
+# Add > Additional overlays and parameters are documented /boot/overlays/README
+dtoverlay=disable-bt
+dtoverlay=disable-wifi
+
+# add > [all]
+enable_uart=1
+
+# Disable Serial Port & Enable Serial Power
+sudo raspi-config
