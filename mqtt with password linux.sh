2d426d68d0602e3b11a2e1fb78bd92c7d820b0ae
Eddie dSuZa <52710883+EddieDSuza@users.noreply.github.com>
2024-11-24 16:20:39 -0300

    Create mqtt with password linux.sh

---
+++ mqtt with password linux.sh
@@ -0,0 +1,17 @@
+#MQTT Install
+sudo apt-get install mosquitto -y
+sudo apt-get install mosquitto-clients
+ 
+#Configure MQTT
+sudo nano /etc/mosquitto/mosquitto.conf
+ 
+#Append these lines to the bottom of the file and save it:
+allow_anonymous false
+password_file /etc/mosquitto/pwfile
+listener 1883
+        
+#Set an MQTT username and password
+ sudo mosquitto_passwd -c /etc/mosquitto/pwfile YOUR-NEW-MQTT-USERNAME
+ 
+#Test the broker
+mosquitto_sub -d -u YOUR-MQTT-USERNAME -P YOUR-MQTT-PASSWORD -t dev/test
